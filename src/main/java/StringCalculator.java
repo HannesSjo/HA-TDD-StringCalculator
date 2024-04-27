98254fd49a383b486b78d83a6e441742210134ab
Hannes <hannes.sjo2@gmail.com>
2024-04-27 17:39:39 +0300

    Did a thing

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,7 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     public int add(String input) {
-        //TODO implement
-        return 0;
+        if (input.isBlank()) return 0;
+
+        List<String> delimiters = new ArrayList<>();
+        delimiters.add("\n");
+        delimiters.add(",");
+        delimiters.add(" ");
+
+        if (input.startsWith("//")){
+            int startIndex = input.indexOf("//") + 2;
+            int endIndex = input.indexOf("\n");
+            String delimiter = input.substring(startIndex, endIndex);
+            delimiters.add(delimiter);
+            input = input.substring(endIndex + 1);
+        }
+
+        StringBuilder regexBuilder = new StringBuilder("[");
+        for (String delimiter : delimiters) {
+            regexBuilder.append(delimiter);
+        }
+        regexBuilder.append("]");
+
+        String[] numbersArray = input.split(regexBuilder.toString());
+        int sum = 0;
+        for (String num : numbersArray) {
+            int intNum = Integer.parseInt(num);
+            if (intNum < 0) throw new IllegalArgumentException("Negatives not allowed " + intNum);
+            sum += intNum;
+        }
+        return sum;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -15,4 +15,30 @@ public class StringCalculatorTest {
     public void testEmptyStringReturnsZero() {
         Assertions.assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void testAddition() {
+        Assertions.assertEquals(1, calculator.add("1"));
+        Assertions.assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    public void testAdditionOfManyNumber() {
+        Assertions.assertEquals(2335, calculator.add("1,5,6,7,2,4,56,32,78,12,32,100,2000"));
+        Assertions.assertEquals(19, calculator.add("1,5,6,7"));
+    }
+
+    @Test
+    public void testUseOfLBs() {
+        Assertions.assertEquals(6, calculator.add("1\n2,3"));
+    }
+    @Test
+    public void testOfSpecialDelimiter() {
+        Assertions.assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void testOfNegativeValue() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.add("2.-5"));
+    }
 }
